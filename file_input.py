d83bfdd99a0034fb32382b6e59ef0b4f7b88f605
JackyFWong <jwong2398@gmail.com>
2019-10-22 13:46:32 -0400

    retabbing

--- file_input.py
+++ file_input.py
@@ -3,25 +3,25 @@ import sys
 
 # parse a json file for given commands
 def parse_file(filepath):
-        data = []
-        with open(filepath, 'r') as file_obj:
-                file_data = json.load(file_obj)
+    data = []
+    with open(filepath, 'r') as file_obj:
+        file_data = json.load(file_obj)
 
-                # conc = concurrent set of commands
-                for conc in file_data:
-                        row = []
+        # conc = concurrent set of commands
+        for conc in file_data:
+            row = []
 
-                        # key being pressed
-                        if conc["type"] == "key":
-                                for keys in conc["cmd"]:
-                                        row.append([ keys["dur"], keys["key"] ])
-                                data.append([ conc["type"], row, conc["num"] ])
+            # key being pressed
+            if conc["type"] == "key":
+                for keys in conc["cmd"]:
+                    row.append([ keys["dur"], keys["key"] ])
+                data.append([ conc["type"], row, conc["num"] ])
 
-                        # comment being printed on console
-                        elif conc["type"] == "comment":
-                                data.append([conc["type"], conc["cmd"]])
+            # comment being printed on console
+            elif conc["type"] == "comment":
+                data.append([conc["type"], conc["cmd"]])
 
-                        else:
-                                sys.exit("invalid input: json error")
+            else:
+                sys.exit("invalid input: json error")
 
-        return data
+    return data
--- virtual_key.py
+++ virtual_key.py
@@ -11,60 +11,60 @@ wait = 'f'
 
 # game runs at 60 fps
 def to_frames(num):
-        return num/60
+    return num/60
 
 # function for sorting unordered simulaneous inputs
 def sort_first(val):
-        return val[0]
+    return val[0]
 
 # press keys
 # conc_keys = list of concurrent keys
 # do not have both an f key and something else
 def type_key(conc_keys):
-        # f key is for pausing all actions
-        if conc_keys[0][1] == 'f':
-                        wait_frames(conc_keys[0][0])
-        else:
-                conc_keys.sort(key = sort_first)
+    # f key is for pausing all actions
+    if conc_keys[0][1] == 'f':
+            wait_frames(conc_keys[0][0])
+    else:
+        conc_keys.sort(key = sort_first)
 
-                # gather all simul inputs
-                inputstr = ''
-                for fcmd in conc_keys:
-                        inputstr = inputstr + fcmd[1] + ', '
-                # remove last comma and space
-                inputstr = inputstr[:(len(inputstr)-2)]
-                kb.press(inputstr)
+        # gather all simul inputs
+        inputstr = ''
+        for fcmd in conc_keys:
+            inputstr = inputstr + fcmd[1] + ', '
+        # remove last comma and space
+        inputstr = inputstr[:(len(inputstr)-2)]
+        kb.press(inputstr)
 
-                # release buttons, shortest duration first
-                # fcmd = frame, command
-                for fcmd in conc_keys:
-                        waittime = fcmd[0]
-                        wait_frames(waittime)
-                        kb.release(fcmd[1])
-                        del fcmd
-                        # update other wait times according to current
-                        for others in conc_keys:
-                                others[0] -= waittime
+        # release buttons, shortest duration first
+        # fcmd = frame, command
+        for fcmd in conc_keys:
+            waittime = fcmd[0]
+            wait_frames(waittime)
+            kb.release(fcmd[1])
+            del fcmd
+            # update other wait times according to current
+            for others in conc_keys:
+                others[0] -= waittime
 
 # sleep for a given num of frames
 def wait_frames(frames):
-        time.sleep(to_frames(frames))
+    time.sleep(to_frames(frames))
 
 if __name__ == '__main__':
-        filepath = infile
-        if (len(sys.argv) == 2):
-                data = fin.parse_file("games/"+sys.argv[1])
+    filepath = infile
+    if (len(sys.argv) == 2):
+        data = fin.parse_file("games/"+sys.argv[1])
+    else:
+        print("Invalid Arguments: Only 1 argument (file location) is required `folder/file_name.json`")
+    for elem in data:
+        # action
+        if elem[0] == "key":
+            i = elem[2]
+            while i > 0:
+                type_key(copy.deepcopy(elem[1]))
+                i -= 1
+        # comment printed on console
+        elif elem[0] == "comment":
+            print(elem[1])
         else:
-                print("Invalid Arguments: Only 1 argument (file location) is required `folder/file_name.json`")
-        for elem in data:
-                # action
-                if elem[0] == "key":
-                        i = elem[2]
-                        while i > 0:
-                                type_key(copy.deepcopy(elem[1]))
-                                i -= 1
-                # comment printed on console
-                elif elem[0] == "comment":
-                        print(elem[1])
-                else:
-                        sys.exit('invalid input: element type error')
+            sys.exit('invalid input: element type error')
